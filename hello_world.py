434b6d5e7a2cf5d8c7c39d29db08784f9daf327f
Toby Dixon <thetjdixon1234@gmail.com>
2020-10-27 18:17:02 +0000

    Add hello_world.py Removed system print information and replaced with a class that loads the environment model from ShowBase. The scene is then scaled and has its position transformed.

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,17 @@
-import sys
-import platform
+from direct.showbase.ShowBase import ShowBase
 
-print("hello")
+class MyApp(ShowBase):
 
-print(1, sys.version)
-print(2, platform.python_implementation())
-print(3, sys.executable)
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        # Load the environment model
+        self.scene = self.loader.loadModel("models/environment")
+        # Reparent the model to render
+        self.scene.reparentTo(self.render)
+        # Apply scale and position transforms on the model
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(-8, 42, 0)
+
+app = MyApp
+app.run()
